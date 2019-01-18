da755400f15b86b13a1c8743dcacd5d5f9db6f6f
LemmoNade <37378615+LemmoNadeYT@users.noreply.github.com>
2019-01-18 11:31:15 -0500

    Finished

--- Items/MagicStaff.cs
+++ Items/MagicStaff.cs
@@ -5,20 +5,20 @@ using Terraria.ModLoader;
 
 namespace HeroSickle.Items
 {
-	public class FireStaff : ModItem
+	public class MagicStaff : ModItem
 	{
 		public override void SetStaticDefaults()
 		{
-                        DisplayName.SetDefault("Flame Staff");
-			Tooltip.SetDefault("Shoots a fire wave");
+                        DisplayName.SetDefault("Soul Staff");
+			Tooltip.SetDefault("Shoots a Soul Flame!");
 			Item.staff[item.type] = true;
 		}
 		public override void SetDefaults()
 		{
-			item.damage = 30;
+			item.damage = 8;
 			item.magic = true;
-			item.width = 999;
-			item.height = 999;
+			item.width = 40;
+			item.height = 32;
 			item.useTime = 9;
 			item.useAnimation = 9;
 			item.useStyle = 5;
@@ -28,15 +28,16 @@ namespace HeroSickle.Items
 			item.rare = 3;
 			item.UseSound = SoundID.Item11;
 			item.autoReuse = true;
-			item.mana = 8;
-                        item.shoot = mod.ProjectileType<FireStaffP>();
-			item.shootSpeed = 12f;
+			item.mana = 3;
+                        item.shoot = mod.ProjectileType<SoulFlameP>();
+			item.shootSpeed = 7f;
 		}
 		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
-                        recipe.AddIngredient(ItemID.HellstoneBar, 8);
-                        recipe.AddTile(TileID.Anvils);
+                        recipe.AddIngredient(ItemID.Wood, 8);
+			                        recipe.AddIngredient(null, "SoulFire", 10);
+                        recipe.AddTile(TileID.WorkBenches);
 			recipe.SetResult(this);
 			recipe.AddRecipe();
 		}
